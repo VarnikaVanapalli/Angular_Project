6185d6bbaadcff61cd689e12ff2136593f267d4e
VarnikaVanapalli <nagavarnikassd2004@gmail.com>
2024-11-27 12:35:56 +0530

    backend

---
+++ Backend/src/main/java/com/bbs/Bus_Booking_Springboot/BusBookingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.bbs.Bus_Booking_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusBookingSpringbootApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/bbs/Bus_Booking_Springboot/BusBookingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbs.Bus_Booking_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusBookingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
